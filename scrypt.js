bfdbb6b331d7b610e0430632cff3d0af38a45f38
Matifernandezar <121912590+Matifernandezar@users.noreply.github.com>
2025-01-29 14:36:27 -0300

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener('submit', function(e) {
         e.preventDefault();
         const name = document.getElementById('name').value;
-        const whatsappNumber = '5492974617905'; // Reemplaza con tu número de WhatsApp
+        const whatsappNumber = '5492975815752'; // Reemplaza con tu número de WhatsApp
         const message = `Hola, mi nombre es ${name}. Me gustaría tener más información para poder jugar.`;
         const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
         window.open(url, '_blank');
